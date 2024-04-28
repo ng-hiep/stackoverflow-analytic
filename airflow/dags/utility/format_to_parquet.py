475f21d57a78875ae7f70b8966c68613647a5e6f
Hiep Nguyen <nguyen.tph128@gmail.com>
2024-04-28 21:31:15 +0700

    feat: create utility function

---
+++ airflow/dags/utility/format_to_parquet.py
@@ -0,0 +1,10 @@
+import logging
+import pandas as pd
+
+def parquetize(src_file, path):
+    if not src_file.endswith('.csv'):
+        logging.error("Can only accept source files in CSV format, for the moment")
+        return
+
+    table = pd.read_csv(src_file)
+    table.to_parquet(path)
